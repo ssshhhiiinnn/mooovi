3afc8df2734961e7f191dfd303a996513c034dc3
ssshhhiiinnn <shin.3534.a@gmail.com>
2017-10-13 18:39:34 +0900

    前ページのスクレイピングを可能化

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -27,14 +27,52 @@
 #
 # end
 
+# ーーー模範解答ーーー
 
+# class Scraping
+#   def self.movie_urls
+#     links = []
+#     agent = Mechanize.new
+#     current_page = agent.get("http://review-movie.herokuapp.com/")
+#     elements = current_page.search('.entry-title a')
+#     elements.each do |ele|
+#       links << ele.get_attribute('href')
+#     end
+#
+#     links.each do |link|
+#       get_product('http://review-movie.herokuapp.com/' + link)
+#     end
+#   end
+#
+#   def self.get_product(link)
+#     agent = Mechanize.new
+#     page = agent.get(link)
+#     title = page.at('.entry-title').inner_text
+#     image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
+#     product = Product.new(title: title, image_url: image_url)
+#     product.save
+#   end
+# end
+
+
+
+class Scraping
   def self.movie_urls
     links = []
     agent = Mechanize.new
-    current_page = agent.get("http://review-movie.herokuapp.com/")
-    elements = current_page.search('.entry-title a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
+
+    next_url = ""
+
+    while true
+      current_page = agent.get("http://review-movie.herokuapp.com/" + next_url)
+      elements = current_page.search('.entry-title a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.pagination .next a')
+      break unless next_link
+      next_url = next_link.get_attribute('href')
     end
 
     links.each do |link|
@@ -42,13 +80,13 @@
     end
   end
 
+
   def self.get_product(link)
     agent = Mechanize.new
     page = agent.get(link)
     title = page.at('.entry-title').inner_text
     image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
-
-    product = Product.new(title: title, image_url: image_url)
+     product = Product.where(title: title, image_url: image_url).first_or_initialize
     product.save
   end
 end
