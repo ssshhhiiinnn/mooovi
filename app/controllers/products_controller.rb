dd2ebf0627102c17e800904f7cd4b8cca1e8fe87
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-14 22:04:23 +0900

    indexページの表示を、各映画の画像とタイトルが表示されるようにした

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 class ProductsController < RankingController
   def index
-    # productsテーブルから最新順に作品を２０件取得する
-    @products = []
+    @products = Product.order('id ASC').limit(20)
   end
 
   def show
-    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
+    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて
     @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
   end
 
