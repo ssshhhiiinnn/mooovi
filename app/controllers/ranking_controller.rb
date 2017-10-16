9dd6c772ebe6d341846f22e5f34db8e43756d4ec
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-16 15:00:44 +0900

    ランキング機能の実装

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,3 +1,8 @@
 class RankingController < ApplicationController
   layout 'review_site'
+  before_action :ranking
+
+  def ranking
+    @ranking = Product.limit(5)
+  end  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :reviews
+
+  def review_average
+    # self.reviews.average(:rate).round
+    reviews.average(:rate).round 
+  end
 end
