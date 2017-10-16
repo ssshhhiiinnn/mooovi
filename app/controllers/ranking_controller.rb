170739e5ebc390a5ab546805a954cdd279b6dccf
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-16 15:36:04 +0900

    ランキング機能の実装（内部ロジックまで完成版）

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,6 +3,9 @@ class RankingController < ApplicationController
   before_action :ranking
 
   def ranking
-    @ranking = Product.limit(5)
-  end  
+    # @ranking = Product.limit(5)
+
+    product_ids = Review.group(:product_id).order('count_product_id DESC').limit(5).count(:product_id).keys
+    @ranking = product_ids.map{|id| Product.find(id)}
+  end
 end
